74f8819a5d59b08158c862d8c4250244895f720a
Danielle Gellis <danisyellis@gmail.com>
2017-09-19 15:46:43 -0700

    setup immutable

--- src/models/db/db.js
+++ src/models/db/db.js
@@ -1,10 +1,11 @@
 const pgp = require("pg-promise")();
+const { Map } = require('immutable');
 const config = require("../../config/config").getConfig();
 
 const connectionObject = {
-  host: config.db.host,
-  port: config.db.port,
-  database: config.db.name
+  host: config.get("db").host,
+  port: config.get("db").port,
+  database: config.get("db").name
 };
 
 const db = pgp(connectionObject);
